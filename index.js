caf79553b258e00583ef0a0bf418fe16af911ba2
sergei <1351618@gmail.com>
2023-10-25 02:14:33 +0300

    rotate

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+if (/Mobi|Android/i.test(navigator.userAgent)) {
+  console.log("MOBILE");
+} else {
+  console.log("PC");
+}
+
 const sides = document.querySelectorAll(".side"); //стороны контроллера
 const thumbSticks = document.querySelector(".thumb-sticks");
+const spaceBtn = document.querySelector(".space-btn");
+const space = document.querySelector(".space");
 
 const keyActions = {
   ArrowUp: "up",
   ArrowDown: "down",
   ArrowLeft: "left",
   ArrowRight: "right",
+  " ": "space",
 };
 
 document.addEventListener("keydown", (event) => {
   if (keyActions[event.key]) {
-    // console.log(keyActions[event.key]);
+    console.log(keyActions[event.key]);
+
     sides.forEach((item) => {
       if (item.classList.contains(keyActions[event.key])) {
         item.classList.toggle("hide");
@@ -41,6 +51,14 @@ document.addEventListener("keydown", (event) => {
       thumbSticks.style.left = "70%";
       setTimeout(() => {
         thumbSticks.style.left = "50%";
+      }, 150);
+    }
+    if (keyActions[event.key] === "space") {
+      spaceBtn.style.transform = "translate(0px, 0px)";
+      space.style.boxShadow = "1px -1px 1px #fff, -1px 2px 3px #000";
+      setTimeout(() => {
+        spaceBtn.style.transform = "translate(1px, -1px)";
+        space.style.boxShadow = "1px -1px 1px #fff, -2px 4px 6px #000";
       }, 150);
     }
   }
